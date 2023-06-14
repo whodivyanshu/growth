9fefdad15481ea02330e68045feb766f88430348
DivyanshuLohar <divyanshulohar2002@gmail.com>
2023-06-14 20:53:12 +0530

    comiit

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,16 @@
 import Image from 'next/image'
 import styles from "./page.module.css"
 import Title from '@/components/body1/Title'
+
 import Properties from '@/components/properties/Properties'
+import Body2 from '@/components/body2/Body2'
 
 export default function Home() {
 
   return (
     <>
       <Title/>
-      {/* <Body2 /> */}
+      <Body2 />
       <Properties />
     </>
   )
