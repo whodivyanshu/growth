5399e9219202138aae439ca3154204375e18213c
Aditya Sharma <sharma.aditya19@outlook.com>
2023-06-18 05:43:37 +0000

    navbar functionality and css bug handled

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -112,13 +112,20 @@ const Navbar = () => {
                         <Link href="/">
                         <li className={styles.drawerItem}>Home</li>
                             </Link>
-                        <li className={styles.drawerItem}>Investments</li>
-                        <li className={styles.drawerItem}>FAQ</li>
-                        <li className={styles.drawerItem}>About Us</li>
-                        <li className={styles.drawerItem}>
+                        <Link href="/properties">
+                            
+                    <li className={styles.navbarItem}>Investments</li>
+                            </Link>
+                        <Link href="/faq">
+                    <li className={styles.navbarItem}>FAQ</li>
+                        </Link>
+                        <Link href="/about">
+                    <li className={styles.navbarItem}>About Us</li>
+                        </Link>
+                    <li className={styles.navbarItem}>
                             <button className={styles.drawerbtn1}>Refer and Earn</button>
                         </li>
-                        <li className={`${styles.drawerItem} ${styles.support}`}>
+                        <li className={`${styles.drawerItem} ${styles.support}`}onClick={toggleSupport}>
                             <Image src="https://img.icons8.com/ios/90/4E0668/headset--v1.png" width={30} height={30} alt='customer care' />
                         </li>
                         <li className={styles.drawerItem}>
