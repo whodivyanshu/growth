16549fcfc6189766ec701334051b5cdb67df3fc9
DivyanshuLohar <divyanshulohar2002@gmail.com>
2023-06-18 14:30:38 +0530

    commit by divyanshu

--- src/components/properties/property/Property.jsx
+++ src/components/properties/property/Property.jsx
@@ -1,45 +1,45 @@
 "use client"
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import styles from "./property.module.css";
 import Image from 'next/image';
 import Link from 'next/link';
-
-
+import signinContext from '@/context/signin/signinContext';
 
 const Property = (props) => {
+  const log = useContext(signinContext);
 
   return (
-    <Link href={{ pathname: "/property" }} key={props.id}>
-   <div className={styles.container} >
-    <div className={styles.image}>
-      <p>{props.location}<br /> {props.area}</p>
-
-    </div>
-    <p className={styles.unit}>1 Unit</p>
-    <hr className={styles.hr} />
-    <div className={styles.main}>
-      <h3>Min. investment: ₹{props.investment}</h3>
-      <p>Funded: ₹{props.funded} (100%)</p>
-      <hr className={styles.hrr} />
-      <h5>GENERATING RETURNS</h5>
-      {/* <h5>{props.id}</h5> */}
-      <hr className={styles.hr} />
-      <div className={styles.iteminvest}>
-        <div className={styles.investleft}>
-          
-            <span style={{fontFamily: "fantasy"}}>Investors <br />  <span style={{fontWeight: "bold"}} >{props.investors}</span></span>
+    log.login ? (
+      <Link href={{ pathname: "/property" }} key={props.id}>
+        <div className={styles.container}>
+          <div className={styles.image}>
+            <p>{props.location}<br /> {props.area}</p>
+          </div>
+          <p className={styles.unit}>1 Unit</p>
+          <hr className={styles.hr} />
+          <div className={styles.main}>
+            <h3>Min. investment: ₹{props.investment}</h3>
+            <p>Funded: ₹{props.funded} (100%)</p>
+            <hr className={styles.hrr} />
+            <h5>GENERATING RETURNS</h5>
+            {/* <h5>{props.id}</h5> */}
+            <hr className={styles.hr} />
+            <div className={styles.iteminvest}>
+              <div className={styles.investleft}>
+                <span style={{ fontFamily: "fantasy" }}>Investors <br />  <span style={{ fontWeight: "bold" }}>{props.investors}</span></span>
+              </div>
+              <div className={styles.investright}>
+                <span style={{ fontFamily: "fantasy" }}>Returns (CAGR)
+                  <br /> <span style={{ fontWeight: "bold" }}>{props.returns}%</span></span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.investright}>
-        <span style={{fontFamily: "fantasy"}} >Returns (CAGR)
- <br /> <span style={{fontWeight: "bold"}} >{props.returns}%</span></span>
-    
-
-        </div>
-      </div>
-    </div>
-   </div>
-   </Link>
-  )
+      </Link>
+    ) : (
+        alert("Please Sign In to view the properties")
+      )
+  );
 }
 
-export default Property
+export default Property;
