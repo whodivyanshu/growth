90edb9eab43b91bbade87773010e18a7652ed517
DivyanshuLohar <divyanshulohar2002@gmail.com>
2023-06-20 21:00:37 +0530

    Update Property.jsx

--- src/components/properties/property/Property.jsx
+++ src/components/properties/property/Property.jsx
@@ -3,32 +3,32 @@ import styles from "./property.module.css";
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Property = () => {
+const Property = (props) => {
   return (
     <Link href="/property">
       <div className={styles.contain}>
 
    <div className={styles.container}>
     <div className={styles.image}>
-      <p>Herbon Avenue <br /> Bangalore</p>
+      <p>{props.location} <br /> {props.area}</p>
 
     </div>
     <p className={styles.unit}>1 Unit</p>
     <hr className={styles.hr} />
     <div className={styles.main}>
-      <h3>Min. investment: ₹5,000</h3>
-      <p>Funded: ₹1,17,05,000 (100%)</p>
+      <h3>Min. investment: ₹{props.investment}</h3>
+      <p>Funded: ₹{props.funded} (100%)</p>
       <hr className={styles.hrr} />
       <h5>GENERATING RETURNS</h5>
       <hr className={styles.hr} />
       <div className={styles.iteminvest}>
         <div className={styles.investleft}>
           
-            <span style={{fontFamily: "fantasy"}}>Investors <br />  <span style={{fontWeight: "bold"}} >160</span></span>
+            <span style={{fontFamily: "fantasy"}}>Investors <br />  <span style={{fontWeight: "bold"}} >{props.investors}</span></span>
         </div>
         <div className={styles.investright}>
         <span style={{fontFamily: "fantasy"}} >Returns (CAGR)
- <br /> <span style={{fontWeight: "bold"}} >13.75%</span></span>
+ <br /> <span style={{fontWeight: "bold"}} >{props.returns}%</span></span>
 
         </div>
       </div>
