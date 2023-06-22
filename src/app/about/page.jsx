df58be1aa22c4547180fb8f2ee23f30cc4e00302
Aditya Sharma <sharma.aditya19@outlook.com>
2023-06-22 06:36:33 +0530

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from './about.module.css';
 import TopRight from '../../../public/about-top-right.svg';
-import BottomRight from '../../../public/about-bottom-right.png';
 import Image from 'next/image';
 import Link from 'next/link';
+import FirstImage from 'public/growth2.svg';
+import SecondImage from 'public/growth4.svg';
 
 const About=()=>{
     return(
@@ -11,12 +12,12 @@ const About=()=>{
             <div className={styles.flexDiv} id={styles.top}>
                 <div>
                     <h1>Simple.</h1>
-                    <h1 style={{color:"rgb(78, 6, 104)"}}>Transparent.</h1>
+                    <h1 id={styles.highlight}>Transparent.</h1>
                     <h1>Data First.</h1>
                     <br />
                     <p>Aasthy is democratizing real estate investments to bridge the multi-trillion dollar gap between high infrastructure demand and low retail investments due to lack of transparency and access. We do this with our deep experience in technology, data and investment structures.</p>
                 </div>
-                <div><Image className={styles.image} width={400} height={400} src={TopRight} alt="image"></Image></div>
+                <div><Image className={styles.image} width={400} height={400} src={FirstImage} alt="image"></Image></div>
             </div>
             <div className={styles.flexDiv} id={styles.bottom}>
                 <div>
@@ -27,7 +28,7 @@ const About=()=>{
                     <br />
                     <Link type="button" id={styles.exploreAss} href="/#properties">Explore Assets!</Link>
                 </div>
-                <div><br /><br /><Image className={styles.image} width={380} height={240} src={BottomRight} alt="image"></Image></div>
+                <div><br /><br /><Image className={styles.image} width={380} height={240} src={SecondImage} alt="image"></Image></div>
             </div>
         </div>
     )
